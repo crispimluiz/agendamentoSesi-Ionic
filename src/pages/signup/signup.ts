9edd3027cd70bf938869b6f618649e0e065482aa
crispimluiz <crispimluiz@live.com>
2019-09-06 00:04:20 -0300

    Controlando o Formulário

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -8,7 +9,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SignupPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  formGroup: FormGroup;
+
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams,
+    public formBuilder: FormBuilder) {
+
+      this.formGroup = this.formBuilder.group({
+        nome:['', [Validators.required, Validators.minLength(5), Validators.minLength(120)]],
+        cpf:['', [Validators.required, Validators.minLength(11), Validators.maxLength(11)]],
+        email:['', [Validators.required, Validators.email]],
+        pessoaResponsavel:['', Validators.required],
+        dataNascimento: ['', Validators.required],
+        observacao: ['',[]],
+        celular:['', Validators.required],
+        celular2:['', []],
+        senha:['', [Validators.required]]
+
+
+      })
+
   }
 
   signupUser(){
